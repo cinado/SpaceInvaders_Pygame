0fee7234195fc9c42ee3aa9b142bbc22a08c056a
cinado <>
2023-02-17 02:45:17 +0100

    Added background and logo to images. Changes in main.py: Window for game opens when starting main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import os
 import pygame
 
+HEIGHT = 750
+WIDTH = 700
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame Space Invaders")
+pygame.display.set_icon(pygame.image.load(os.path.join("images", "logo.png")))
+
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("images", "background.png")), (WIDTH, HEIGHT))
+
 
 def main():
-    print("initial")
+    running_game = True
+
+    while running_game:
+        WINDOW.blit(BACKGROUND, (0, 0))
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                running_game = False
+                pygame.quit()
+
+
+main()
